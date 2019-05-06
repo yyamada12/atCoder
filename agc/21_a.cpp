1f1af0bbc3f37497da4741c9168c2e69274c2345
yyamada12 <12yacropolisy@gmail.com>
2019-05-07 08:26:10 +0900

    AGC21

---
+++ agc/21_a.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+#include <string.h>
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define ALL(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+ll N;
+
+int main(){
+  cin >> N;
+  int ans = 0;
+  bool flag = true;
+  while(N / 10 > 0) {
+    if(N % 10 != 9) flag = false;
+    N /= 10;
+    ans += 9;
+  }
+
+  ans += N - 1;
+  if(flag) ans++;
+  
+  cout << ans << endl;
+
+  return 0;
+}
+
