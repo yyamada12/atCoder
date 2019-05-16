17e3d08ffc34af337b20bf517249652fbdc936b8
yyamada12 <12yacropolisy@gmail.com>
2019-05-16 23:05:47 +0900

    AGC 1

---
+++ agc/1_b.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+ll N, X;
+
+int main(){
+  cin >> N >> X;
+  ll a, b;
+  if(X > N - X) {
+    a = X;
+    b = N - X;
+  } else {
+    a = N - X;
+    b = X;
+  }
+  ll ans = N;
+
+  while(1) {
+    ll c = a % b;
+    ans += (a / b) * 2 * b;
+    if(c == 0) {
+      ans -= b;
+      break;
+    }
+    a = b;
+    b = c;
+  }
+  
+  cout << ans << endl;
+  return 0;
+}
