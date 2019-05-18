45b93ce8eacfc0d97b0e4012e6f076b37be0d39d
yyamada12 <12yacropolisy@gmail.com>
2019-05-19 07:40:00 +0900

    AGC 3

---
+++ agc/3_a.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+string s;
+
+int main(){
+  cin >> s;
+  int n = s.size();
+  bool flag[4];
+  REP(i, 4) flag[i] = false;
+  REP(i, n) {
+    switch(s[i]){
+      case 'N':
+      flag[0] = true;
+      break;
+      case 'W': 
+      flag[1] = true;
+      break;
+      case 'S': 
+      flag[2] = true;
+      break;
+      case 'E':
+      flag[3] = true;
+      break;
+    }
+  }
+
+  if(flag[0] == flag[2] && flag[1] == flag[3]) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+
+
+
+  return 0;
+}
---
+++ agc/3_c.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+int N;
+const int MAX_N = 100000;
+ll A[MAX_N];
+
+int main(){
+  cin >> N;
+  REP(i, N) cin >> A[i];
+  map<ll, int> mp;
+  REP(i, N) {
+    mp[A[i]] = i;
+  }
+  sort(A, A + N);
+  int ans2 = 0;
+  REP(i, N) {
+    if(mp[A[i]] % 2 != i % 2) ans2++;
+  }
+  cout << ans2 / 2 << endl;
+
+
+  return 0;
+}
+
