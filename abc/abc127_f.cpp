c3a34b5ae33bebcb5ab82198784737c81106a43f
yyamada12 <12yacropolisy@gmail.com>
2019-05-27 21:36:48 +0900

    ABC 127

---
+++ abc/abc127_f.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) do{sort(v.begin(), v.end()); v.erase( unique(v.begin(), v.end()), v.end() );} while(0)
+
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+int Q;
+const int MAX_Q = 200000;
+
+int main(){
+  cin >> Q;
+
+  int q;
+  ll a, b;
+
+  cin >> q >> a >> b;
+
+  ll s = b;
+  priority_queue<ll> fro;
+  priority_queue<ll, vector<ll>, greater<ll>> bac;
+  fro.push(a);
+  bac.push(a);
+  bool odd = true;
+
+  REP(i, Q - 1) {
+    cin >> q;
+    if(q == 1) {
+      cin >> a >> b;
+      fro.push(a);
+      bac.push(a);
+
+      ll ft = fro.top(), bt = bac.top();
+      if(ft > bt) {
+        fro.pop();
+        bac.pop();
+        fro.push(bt);
+        bac.push(ft);
+        s += ft - bt;
+      }
+      s += b;
+    } else {
+      cout << fro.top() << " " << s << endl;
+    }
+  }
+
+  return 0;
+}
