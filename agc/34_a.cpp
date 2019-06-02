0c4087a4624be751cbbc1b92a0d09fb89a65ff2f
yyamada12 <12yacropolisy@gmail.com>
2019-06-03 08:39:08 +0900

    AGC 34

---
+++ agc/34_a.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) do{sort(v.begin(), v.end()); v.erase( unique(v.begin(), v.end()), v.end() );} while(0)
+
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+ll N, A, B, C, D;
+string S;
+
+int main(){
+  cin >> N >> A >> B >> C >> D;
+  cin >> S;
+  int n = S.size();
+  for(int i = A - 1; i < C - 1; i++) {
+      if(S[i] == '#' && S[i + 1] == '#') {
+        cout << "No" << endl;
+        return 0;
+      }
+  }
+  for(int i = B - 1; i < D- 1; i++) {
+      if(S[i] == '#' && S[i + 1] == '#') {
+        cout << "No" << endl;
+        return 0;
+      }
+  }
+
+  if(C < D) {
+    cout << "Yes" << endl;
+    return 0;
+  }
+
+  // cout << S << endl;
+
+  for(int i = B - 1; i < D; i++) {
+    // cout << "i: " << i << ", S[i - 1]: " << S[i - 1] << ", S[i]: " << S[i] << ", S[i + 1]: " << S[i + 1] << endl;
+    if(S[i - 1] == '.' && S[i] == '.' && S[i + 1] == '.') {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
+
+  return 0;
+}
+
---
+++ agc/34_b.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define RREP(i,n) for(int i = n - 1; i >= 0; i--)
+
+#define ALL(v) v.begin(),v.end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+#define UNIQUE(v) do{sort(v.begin(), v.end()); v.erase( unique(v.begin(), v.end()), v.end() );} while(0)
+
+
+#define y0 y3487465
+#define y1 y8687969
+
+#define m0(x) memset(x,0,sizeof(x))
+#define m1(x) memset(x,63,sizeof(x))
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+string s;
+
+int main(){
+  cin >> s;
+  vector<string> v;
+  REP(i, s.size()) {
+    if(s[i] == 'A') {
+      continue;
+    } else if(s[i] == 'B' && i < s.size() - 1 &&  s[i + 1] == 'C') {
+      i++;
+    } else {
+      s[i] = 'D';
+    }
+  }
+  s += 'D';
+
+  int i = 0;
+  int prev = 0;
+  REP(i, s.size()) {
+    if(s[i] == 'D') {
+      v.pb(s.substr(prev, i - prev + 1));
+      prev = i;
+    }
+  }
+
+  ll ans = 0;
+  REP(i, v.size()){
+    int ct = 0;
+    REP(j, v[i].size()){
+      if(v[i][j] == 'A') ct++;
+      else if (v[i][j] == 'B') ans += ct;
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
+
